9d29c671db010ba7d61123765ac0054895623e7e
Priit Paluoja <priit.paluoja@cern.ch>
2017-09-21 11:08:31 +0300

    Tomcat support

--- src/main/java/ee/scanner/tablet/TabletScannerApplication.java
+++ src/main/java/ee/scanner/tablet/TabletScannerApplication.java
@@ -3,10 +3,18 @@ package ee.scanner.tablet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TabletScannerApplication {
-    public static void main(String[] args) {
+public class TabletScannerApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(TabletScannerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TabletScannerApplication.class);
     }
 }
