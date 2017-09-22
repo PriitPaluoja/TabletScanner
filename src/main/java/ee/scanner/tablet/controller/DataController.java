bca9d4b248d68cca293104ea300d2a9ab98b24c5
Priit Paluoja <priit.paluoja@cern.ch>
2017-09-22 11:36:20 +0300

    Roles based management

--- src/main/java/ee/scanner/tablet/controller/DataController.java
+++ src/main/java/ee/scanner/tablet/controller/DataController.java
@@ -9,6 +9,7 @@ import ee.scanner.tablet.service.DataSaveService;
 import ee.scanner.tablet.util.ControllerUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,6 +21,7 @@ import javax.validation.Valid;
 
 @Controller
 @AllArgsConstructor(onConstructor = @__(@Autowired))
+@PreAuthorize("hasRole('ADMIN')")
 public class DataController {
     private final DataSaveService dataSaveService;
 
