64f0858ca2c8266eb5bfda27c1f7799c08840f6e
Priit Paluoja <priit.paluoja@cern.ch>
2017-12-08 13:31:49 +0200

    Update titles

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -209,7 +209,7 @@ $(document).ready(function () {
         data.addColumn('number', 'Kasutusi');
         data.addRows(hist);
         var options = {
-            title: "Histogramm: päevade arv, kus kasutati X korda seadet päevas",
+            title: "Histogramm: seadmete välja laenutamine päevade vahemike lõikes",
             width: 800,
             height: 500,
             vAxis: {title: "Laenutusi", viewWindowMode: "explicit", viewWindow: {min: 0}},
